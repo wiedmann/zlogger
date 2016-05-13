6aa3d2dddba2e9f06f1cdb94fae7ff75530a3d6b
Christian Wiedmann <christian@mobilehealthconsumer.com>
2016-05-12 21:23:15 -0700

    First implementation of rabbitmq publishing of pos data

--- parse_log.py
+++ parse_log.py
@@ -4,6 +4,7 @@ import json
 import argparse
 import sqlite3
 import traceback
+import pika
 
 import mysql.connector
 from mysql.connector import errors as mysql_errors
@@ -63,8 +64,17 @@ def main(argv):
     parser.add_argument('-P', '--mysql_password', help='mysql password')
     parser.add_argument('-d', '--debug', action='store_true', help='Debug things')
     parser.add_argument('-i', '--update_interval', type=int, help='chalkline update interval', default=30)
+    #parser.add_argument('--pika_url', default='amqp://guest:guest@localhost:5672/%2F')
+    parser.add_argument('--pika_url')
     args = parser.parse_args()
     line_mapper = LineMapper()
+
+    if args.pika_url:
+        parameters = pika.URLParameters(args.pika_url)
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
+    else:
+        channel = None
 
     with open(args.zlogger_file) as lfile:
         loglines = follow(lfile)
@@ -72,67 +82,78 @@ def main(argv):
         read_chalklines(dbh, line_mapper)
         mycursor = dbh.cursor()
         last_line_update={}
-        for line in loglines:
-            line = line.strip()
-            try:
-                data = json.loads(line)
-                while True:
-                    if not dbh:
-                        dbh = opendb(args)
-                        mycursor = dbh.cursor()
-                    try:
-                        if data['e'] == 'LINE':
-                            line_id = data['v']['line']
-                            line_name = data['v']['name']
-                            line_data = str(data['v']['data'])
-                            if args.debug:
-                                print "Line id %s - %s" % (line_id, line_name)
-                            if not line_mapper.add_source_line(line_id, line_name):
-                                print "Adding new chalkline to db: %s, '%s', '%s'" % (line_id, line_data, line_name)
-                                mycursor.execute("INSERT into chalkline (data, name) VALUES (%s, %s)",
-                                                 (line_data, line_name))
-                                mycursor.execute("SELECT line from chalkline where name = %s", (line_name, ))
-                                line_id = mycursor.fetchall()[0][0]
-                                line_mapper.add_dest_line(line_id, line_name)
-                        elif data['e'] == 'NEARBY':
-                            line_id = line_mapper.get_mapping(data['v']['data'])
-                            mark_chalkline_active(dbh, line_id)
-                            last_line_update[line_id] = time.time()
-                        elif data['e'] == 'POS':
-                            try:
-                                value = data['v']
-                                line_id = line_mapper.get_mapping(value['line'])
-                                if line_id not in last_line_update or (time.time() - last_line_update[line_id]) > args.update_interval:
-                                    mark_chalkline_active(dbh, line_id)
-                                    last_line_update[line_id] = time.time()
-                                params = (data['msec'], value['id'], line_id, value['fwd'], value['m'],
-                                          value['mwh'], value['dur'], value['ele'], value['spd'], value['hr'],
-                                          value['obs'])
+        try:
+            for line in loglines:
+                line = line.strip()
+                try:
+                    data = json.loads(line)
+                    while True:
+                        if not dbh and not args.no_db:
+                            dbh = opendb(args)
+                            mycursor = dbh.cursor()
+                        try:
+                            if data['e'] == 'LINE':
+                                line_id = data['v']['line']
+                                line_name = data['v']['name']
+                                line_data = str(data['v']['data'])
                                 if args.debug:
-                                    print "Msec=%s,ID=%s,Line=%s,fwd=%s,meters=%s,mwh=%s,duration=%s,Elevation=%s,Speed=%s,HR=%s,monitor=%s" % params
-                                SQL = '''REPLACE INTO live_results (msec, riderid, lineid, fwd, meters, mwh, duration,
-                                        elevation, speed, hr, monitorid)
-                                        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'''
-                                mycursor.execute(SQL, params)
-                            except KeyError:
-                                print "WARNING - POS Entry for unknown line: %s" % line
-                        break
-                    except mysql_errors.Error:
-                        print "Exception - reopening mysql connection in 3 seconds: %s" % traceback.format_exc()
-                        time.sleep(3)
-                        if dbh:
-                            try:
-                                mycursor.close()
-                                mycursor = None
-                            except:
-                                pass
-                            try:
-                                dbh.close()
-                                dbh = None
-                            except:
-                                pass
-            except ValueError:
-                print "WARNING - bad log file line: '%s'" % line
+                                    print "Line id %s - %s" % (line_id, line_name)
+                                if not line_mapper.add_source_line(line_id, line_name):
+                                    print "Adding new chalkline to db: %s, '%s', '%s'" % (line_id, line_data, line_name)
+                                    mycursor.execute("INSERT into chalkline (data, name) VALUES (%s, %s)",
+                                                     (line_data, line_name))
+                                    mycursor.execute("SELECT line from chalkline where name = %s", (line_name, ))
+                                    line_id = mycursor.fetchall()[0][0]
+                                    line_mapper.add_dest_line(line_id, line_name)
+                            elif data['e'] == 'NEARBY':
+                                line_id = line_mapper.get_mapping(data['v']['data'])
+                                mark_chalkline_active(dbh, line_id)
+                                last_line_update[line_id] = time.time()
+                            elif data['e'] == 'POS':
+                                try:
+                                    value = data['v']
+                                    line_id = line_mapper.get_mapping(value['line'])
+                                    if line_id not in last_line_update or (time.time() - last_line_update[line_id]) > args.update_interval:
+                                        mark_chalkline_active(dbh, line_id)
+                                        last_line_update[line_id] = time.time()
+                                    params = (data['msec'], value['id'], line_id, value['fwd'], value['m'],
+                                              value['mwh'], value['dur'], value['ele'], value['spd'], value['hr'],
+                                              value['obs'])
+                                    try:
+                                        if channel:
+                                            data = dict(zip(('msec', 'riderid', 'lineid', 'fwd', 'meters', 'mwh', 'duration',
+                                                         'elevation', 'speed', 'hr', 'monitorid'), params))
+                                            channel.basic_publish('zlogger', 'POS.%s.%s' % (line_id, value['id']),
+                                                                  json.dumps(data))
+                                    except:
+                                        print "WARNING: exception publishing POS event: %s" % traceback.format_exc()
+                                    if args.debug:
+                                        print "Msec=%s,ID=%s,Line=%s,fwd=%s,meters=%s,mwh=%s,duration=%s,Elevation=%s,Speed=%s,HR=%s,monitor=%s" % params
+                                    SQL = '''REPLACE INTO live_results (msec, riderid, lineid, fwd, meters, mwh, duration,
+                                            elevation, speed, hr, monitorid)
+                                            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);'''
+                                    mycursor.execute(SQL, params)
+                                except KeyError:
+                                    print "WARNING - POS Entry for unknown line: %s" % line
+                            break
+                        except mysql_errors.Error:
+                            print "Exception - reopening mysql connection in 3 seconds: %s" % traceback.format_exc()
+                            time.sleep(3)
+                            if dbh:
+                                try:
+                                    mycursor.close()
+                                    mycursor = None
+                                except:
+                                    pass
+                                try:
+                                    dbh.close()
+                                    dbh = None
+                                except:
+                                    pass
+                except ValueError:
+                    print "WARNING - bad log file line: '%s'" % line
+        except KeyboardInterrupt:
+            connection.close()
 
 if __name__ == '__main__':
     try:
---
+++ subscribe_events.py
@@ -0,0 +1,52 @@
+import time
+import sys
+import json
+import argparse
+import sqlite3
+import traceback
+import pika
+
+import mysql.connector
+from mysql.connector import errors as mysql_errors
+
+def opendb(args):
+    import mysql.connector
+    return mysql.connector.connect(user=args.mysql_user, host=args.mysql_host, database=args.mysql_database,
+                                   password=args.mysql_password, autocommit=True)
+def callback(ch, method, properties, body):
+    print(" [x] %r: %r" % (method.routing_key, body))
+
+def main(argv):
+    parser = argparse.ArgumentParser(description = 'Race Result Generator')
+    parser.add_argument('topics', nargs='+')
+    parser.add_argument('-D', '--mysql_database', help='mysql database')
+    parser.add_argument('-H', '--mysql_host', help='mysql host')
+    parser.add_argument('-U', '--mysql_user', help='mysql user')
+    parser.add_argument('-P', '--mysql_password', help='mysql password')
+    parser.add_argument('-d', '--debug', action='store_true', help='Debug things')
+    parser.add_argument('--pika_url', default='amqp://guest:guest@localhost:5672/%2F')
+    args = parser.parse_args()
+
+    parameters = pika.URLParameters(args.pika_url)
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.exchange_declare(exchange='zlogger', type='topic')
+    result = channel.queue_declare(exclusive=True)
+    queue_name = result.method.queue
+    for t in args.topics:
+        channel.queue_bind(exchange='zlogger', queue=queue_name, routing_key=t)
+
+    try:
+        channel.basic_consume(callback, queue=queue_name, no_ack=True)
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        connection.close()
+
+if __name__ == '__main__':
+    try:
+        main(sys.argv)
+    except KeyboardInterrupt:
+        print "exiting"
+        exit()
+    except SystemExit, se:
+        print "ERROR:", se
